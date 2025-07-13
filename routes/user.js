77b162cd3733f3cf61258de29d0e1240c90e2dc5
Rahi <amarjit123@gmail.com>
2025-07-13 15:23:14 +0530

    file change

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const WrapError = require("../utils/WrapError.js");
-const { saveRedirectUrl } = require("../../Major_Project/middleware.js");
+const { saveRedirectUrl } = require("../middleware.js");
 const UserListing = require("../controllers/user.js");
 
 router
